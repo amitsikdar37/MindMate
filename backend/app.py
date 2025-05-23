2dc352c9271be06dc4f6cf081d216bd9f0905022
Amit Sikdar <sikdara477@gmail.com>
2025-05-23 11:58:21 +0530

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,6 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
-
 @app.route('/')
 def home():
     return 'Welcome to my Flask app!'
@@ -18,4 +16,6 @@ app.register_blueprint(chat_bp, url_prefix="/api/chat")
 app.register_blueprint(mood_bp, url_prefix="/api/mood")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=port)
+    debug = os.getenv("FLASK_ENV") == "development"
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=debug, port=port)
